2dcd75a8b06d6412fa9deaa135ef91141dbf1359
samkalgudi <samkalgudi@gmail.com>
2018-02-12 15:21:18 +0530

    first commit

---
+++ Network_details.go
@@ -0,0 +1,54 @@
+package main
+import (
+"fmt"
+"github.com/rackspace/gophercloud"
+"github.com/rackspace/gophercloud/openstack"
+"github.com/rackspace/gophercloud/pagination"
+"github.com/rackspace/gophercloud/openstack/networking/v2/networks"
+)
+
+func main() {
+
+// Client authentication with the openstack//
+opts := gophercloud.AuthOptions{
+IdentityEndpoint: "http://10.100.60.231:5000/v2.0",
+Username: "osmosis",
+Password: "osmosis",
+TenantID: "b84bd7c16a6e47479b4aa99d81095b71",
+}
+provider, err := openstack.AuthenticatedClient(opts)
+
+if err != nil {
+fmt.Println("AuthenticatedClient")
+fmt.Println(err)
+return
+}
+//intilizing network client
+client, err := openstack.NewNetworkV2(provider, gophercloud.EndpointOpts{
+Region: "RegionOne",
+})
+
+if err != nil {
+fmt.Println("NewNetworkV2 Error")
+fmt.Println(err)
+return
+}
+
+opts2 := networks.ListOpts{}
+pager := networks.List(client, opts2)
+
+pager.EachPage(func(page pagination.Page) (bool, error) {
+networkList, err := networks.ExtractNetworks(page)
+
+        if err != nil {
+        return false, err
+        }
+	for _, n := range networkList{fmt.Println(n.ID, n.Name, n.Status) 
+		// "n" will be a networks.Network
+	}
+        return true, nil
+})
+
+}
+
+
