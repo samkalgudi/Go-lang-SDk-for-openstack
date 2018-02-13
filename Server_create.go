2d60eb7cb41d765055788fe68f0a414f8ef5ce67
samkalgudi <samkalgudi@gmail.com>
2018-02-13 15:25:16 +0530

    second commit

---
+++ Server_create.go
@@ -0,0 +1,42 @@
+package main
+import (
+"fmt"
+"github.com/rackspace/gophercloud"
+"github.com/rackspace/gophercloud/openstack"
+"github.com/rackspace/gophercloud/openstack/compute/v2/servers"
+)
+
+func main() {
+
+// Client authentication with the openstack//
+opts := gophercloud.AuthOptions{
+IdentityEndpoint: "http://10.100.60.231:5000/v2.0",
+Username: "osmosis",
+Password: "osmosis",
+TenantID: "b84bd7c16a6e47479b4aa99d81095b71",
+}
+provider, err := openstack.AuthenticatedClient(opts)
+
+if err != nil {
+fmt.Println("AuthenticatedClient")
+fmt.Println(err)
+return
+}
+//Intilizing Compute client
+client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+	Region: "RegionOne",
+})
+
+//Creating Server......
+server, err := servers.Create(client, servers.CreateOpts{
+  Name:       "Test123",
+  FlavorName: "m1.tiny",
+  ImageName: "cirros",
+}).Extract()
+
+if err != nil {
+  fmt.Println("Unable to create server: ", err)
+}
+fmt.Println("Server ID: ", server.ID)
+
+}
